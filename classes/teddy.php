48d368f6ce61a5042a96c873232cb965dada43ab
Jason Silberman <jasonjt99@gmail.com>
2013-11-10 15:20:54 -0800

    oops
    fixed the name of the text variable in teddy.php

--- classes/teddy.php
+++ classes/teddy.php
@@ -35,7 +35,7 @@ Class Teddy {
 	}
 
 	protected function _encrypt($bear, $string) {
-		$text = (string) $text;
+		$string = (string) $string;
 		$iv = $bear->get_iv();
 		$key = $bear->get_key();
 		$e = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $string, MCRYPT_MODE_CBC, $iv);
@@ -43,7 +43,7 @@ Class Teddy {
 	}
 
 	protected function _decrypt($bear, $string) {
-		$text = (string) $text;
+		$string = (string) $string;
 		$iv = $bear->get_iv();
 		$key = $bear->get_key();
 		$d = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $string, MCRYPT_MODE_CBC, $iv);
